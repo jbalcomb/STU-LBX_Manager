8bc7b406a0851c5dc9383625f8be6025b5b69196
Jim Balcomb <jim.balcomb@gmail.com>
2021-10-04 10:06:47 -0400

    misc. SDL2

---
+++ LbxMgr/ATS/ats_sdl2.h
@@ -0,0 +1,14 @@
+#ifndef ATS_SDL2_H
+#define ATS_SDL2_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void sdl2_test();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* ATS_SDL2_H */
